ce87f21e14ef479265837e498e6ce37809c56720
Ananta Lamichhane <bloomsnruins@gmail.com>
2021-12-21 14:05:29 +0100

    cleanup, now handle time and counter from backend provided timestamp

--- static/index.js
+++ static/index.js
@@ -1,39 +1,23 @@
 $(document).ready(function(){
-  let  prev_code = ""
-    console.log("ok")
-
-
-
-
-    $('#timeout').text("30");
-   let count = 30 - Math.round(new Date() / 1000) % 30
-    let percent = 100.0 - count*3.33
-    console.log(percent)
    setInterval(function(){
        $.ajax({
         type: "GET",
-            url: '/?api=true',
+            url: '/?api=true', // query with the server every second
             success: function(response){
-
-                        let new_code = Object.values(response)[0]
-                        if(new_code+"" !== prev_code){
+                        // since the response is nested json, finding out the time sent from the server
+                        let count = 30 - (Object.values(Object.values(response)[0])[1] % 30)
+                        // count goes to 30 hence percent is 3.33 times that
+                        let percent = count * 3.33
+                            //iterate through each label, code keypair
                             Object.values(response).forEach((element, index) =>{
-                                $('#code'+(index+1).toString()).text(element+"")
-                                prev_code = new_code
-                            })
+                                let code = Object.values(element)[0]
+                                //update the code
+                                $('#code'+(index+1).toString()).text(code+"")
                             }
-
-                            $('#timeout').text(count);
-                            $('.progress-bar').css('width', percent+'%')
-                            percent <1.0?percent=100.0: percent =percent - 3.33
-                            count==0?count=30:count--
-
-                        $('.progress-bar').css('width', percent+'%')//.attr('aria-valuenow', 0);
-
-
-
-
-
+                                //update the countdown timer
+                                $('#timeout').text(count);
+                                //update the progress bar
+                                $('.progress-bar').css('width', percent+'%')
             }
         });
 
